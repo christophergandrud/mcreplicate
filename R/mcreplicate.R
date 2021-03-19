922fe01bc46724cf0b76a81aaf06a0b4c63f61c0
Olivier Binette <olivier.binette@gmail.com>
2021-03-19 12:03:09 -0400

    Add Windows support

--- R/mcreplicate.R
+++ R/mcreplicate.R
@@ -1,42 +1,74 @@
-#' Multi-core replicate. From the rethinking package:
-#' <https://github.com/rmcelreath/rethinking/blob/3b48ec8dfda4840b9dce096d0cb9406589ef7923/R/utilities.r#L206>
+#' Multi-core replicate.
 #'
-#' @param n integer: the number of replications.
+#' Use multiple cores for repeated evaluation of an expression. This also works on Windows using a parallel socket cluster (see notes below regarding Windows-specific usage).
+#'
+#' @usage mc_replicate(n, expr, mc.cores, simplify = "array", ...)
+#'
+#' @param n integer; the number of replications.
 #' @param expr the expression (a language object, usually a call) to evaluate repeatedly.
-#' @param refresh status update refresh interval
-#' @param mc.cores number of cores to use
+#' @param mc.cores number of cores to use.
+#' @param simplify logical or character string. See \link[base]{sapply} for more information.
+#' @param ... additional parameters for usage on Windows.
+#'
+#' @note On Windows, variables and packages needed for code execution must be explicitely specified. By default, all loaded packages are also loaded on the cluster's workers, and all variables from the current environment which do not start with a "." are exported. Use the following optional arguments to control how to populate each worker's environment:
+#' \describe{
+#'   \item{packages}{character vector of packages to require for each worker.}
+#'   \item{varlist}{character vector of variable names to export on each worker. See \link[parallel]{clusterExport} for more information.}
+#'   \item{envir}{Environment from which  to export variables. See \link[parallel]{clusterExport} for more information.}
+#' }
 #'
 #' @examples
-#' one_sim <- function(n, control_prob, rel_effect) {
+#' one_sim <- function(n = 100, control_prob = 0.1, rel_effect = 0.01) {
 #'   treat_prob <- control_prob + (control_prob * rel_effect)
 #'   cy <- rbinom(n = n, size = 1, prob = control_prob)
 #'   ty <- rbinom(n = n, size = 1, prob = treat_prob)
 #'   mean(ty) - mean(cy)
 #'   }
 #'
-#'   mc_replicate(10, one_sim(n = 100, control_prob = 0.1, rel_effect = 0.01))
+#'   mc_replicate(10, one_sim())
 #'
-#' @importFrom parallel mclapply
+#'   # On Windows, when no particular packages or variables are needed:
+#'   mc_replicate(10, one_sim(), packages = NULL, varlist = NULL)
+#'
+#' @references This is inspired from the rethinking package:
+#' <https://github.com/rmcelreath/rethinking/blob/3b48ec8dfda4840b9dce096d0cb9406589ef7923/R/utilities.r#L206
+#'
+#' @importFrom parallel mclapply detectCores makePSOCKcluster clusterExport parLapply stopCluster
 #' @export
-
-mc_replicate <- function(n, expr, refresh = 0.1, mc.cores = 2) {
+mc_replicate <- function(n, expr, simplify = "array", mc.cores = detectCores(), ...) {
     # check if windows and set cores to 1
-    if (.Platform$OS.type == "windows") {
-        mc.cores <- 1
-        message("Only 1 core is supported on Windows.\nRunning sequentially.")
+    if (.Platform$OS.type == "windows" && mc.cores > 1) {
+        cat("Running parallel code on Windows: a parallel socket cluster will be used.")
+        cat("Variables and packages needed for code execution must be explicitely specified. See the help file for more information and current defaults.")
+
+        # Default exports
+        if (missing(varlist)) {
+            varlist = ls(envir=parent.frame())
+        }
+        if (missing(envir)) {
+            envir = parent.frame()
+        }
+        if (missing(packages)) {
+            packages = c(sessionInfo()$basePkgs, names(sessionInfo()$otherPkgs))
+        }
+
+        cl <- parallel::makePSOCKcluster(mc.cores)
+
+        # Export packages
+        clusterEvalQ(cl, sapply(packages, function(package) require(package)))
+
+        # Export variables
+        clusterExport(cl = cl, varlist = varlist, envir = envir)
+
+        result <- parLapply(cl = cl, 1:n, function(i) eval(substitute(expr)))
+        stopCluster(cl)
+
+    } else {
+        result <- mclapply(1:n, function(i) eval(substitute(expr)), mc.cores = mc.cores)
     }
 
-    show_progress <- function(i) {
-        intervaln <- floor(n * refresh)
-        if (floor(i/intervaln) == i/intervaln) {
-            cat(paste("[", i, "/", n, "]\r"))
-        }
-    }
-    result <- simplify2array(mclapply(1:n, eval.parent(substitute(function(i, ...) {
-        if (refresh > 0) show_progress(i)
-        expr
-    })), mc.cores = mc.cores))
-    if (refresh > 0)
-        cat("\n")
-    result
+    if (!isFALSE(simplify) && length(result))
+        simplify2array(result, higher = (simplify == "array"))
+    else result
 }
+
--- README.md
+++ README.md
@@ -1,36 +1,30 @@
 
-# mcreplicate
+# mcreplicate: multi-core replications
 
 <!-- badges: start -->
 
+[![Lifecycle:
+experimental](https://img.shields.io/badge/lifecycle-experimental-orange.svg)](https://www.tidyverse.org/lifecycle/#experimental)
 [![R-CMD-check](https://github.com/christophergandrud/mcreplicate/workflows/R-CMD-check/badge.svg)](https://github.com/christophergandrud/mcreplicate/actions)
 <!-- badges: end -->
 
-## Purpose
-
-Multi-core replication to make Monte Carlo simulation faster more
-easily. Based on the `mcreplicate` function from the rethinking package.
-The [rethinking](https://github.com/rmcelreath/rethinking) package
-requires installing [rstan](https://cran.r-project.org/package=rstan),
-which can be a hurdle, while also not adding capabilities to this
-function.
-
-**Note:** multi-core support is not available on Windows.
+**mcreplicate** adds multi-core functionality to R’s `replicate`
+function. It allows easy parallelization on all platforms, including on
+Windows.
 
 ## Installation
 
 Install the package from GitHub:
 
 ``` r
-xfun::pkg_attach2("remotes")
-
-install_github("christophergandrud/mcreplicate")
+if (!require(remotes)) install.packages("remotes")
+remotes::install_github("christophergandrud/mcreplicate")
 ```
 
 ## Use
 
 `mc_replicate()` works just like `replicate()`, but distributes the
-replications.
+replications on multiple cores
 
 ``` r
 library(mcreplicate)
@@ -47,10 +41,51 @@ one_sim <- function(n, control_prob, rel_effect) {
 
 diff_means <- mc_replicate(10, one_sim(n = 100, control_prob = 0.1, 
                                       rel_effect = 0.01))
-```
 
-``` r
 diff_means
 ```
 
-    ##  [1] -0.01  0.02  0.08  0.02  0.07 -0.01 -0.02  0.01  0.01  0.06
+    ##  [1] -0.04 -0.03  0.00  0.04 -0.03 -0.05  0.02 -0.03 -0.02  0.09
+
+### Windows users
+
+On Windows, **mcreplicate** relies on a parallel socket cluster backend.
+This requires the user to explicitely specify which packages and
+variables should be used to populate the workers’ environments. By
+default, **mcreplicate** attaches all currently loaded packages and all
+variables from the current environment which do not start with a “.”.
+This can be changed using the `packages`, `varlist` and `envir` optional
+arguments. You can learn more on the function’s help file.
+
+#### Example
+
+``` r
+k = 2
+
+# The following works as intended, since the variable "k" is exported by default to each worker.
+mc_replicate(10, rnorm(k))
+```
+
+    ##            [,1]       [,2]       [,3]       [,4]      [,5]      [,6]       [,7]
+    ## [1,] -1.0895637 -0.9291322 -2.2835993 -0.1549158 0.8148598 0.9499048 -0.1366499
+    ## [2,] -0.7789176  0.4655108  0.8965296  1.0131903 0.6792686 0.4009201 -1.2437706
+    ##           [,8]        [,9]       [,10]
+    ## [1,] 0.6370209 -0.85375635 -0.09725082
+    ## [2,] 0.6277709  0.04385849  1.38561286
+
+``` r
+# For a reduced overhead, you can specify to *only* export the variable "k" from the current environment and to not load any particular package.
+mc_replicate(10, rnorm(k), packages=NULL, varlist=c("k"), envir=environment())
+```
+
+    ##             [,1]       [,2]       [,3]       [,4]       [,5]       [,6]
+    ## [1,]  0.09733941  0.1128511  0.4345149  0.5349322  0.3969553  0.5870589
+    ## [2,] -1.04747904 -0.2757685 -1.1179181 -0.5689403 -1.7737434 -1.9930214
+    ##            [,7]       [,8]      [,9]     [,10]
+    ## [1,] -0.7326846 -0.1059534 0.3731130  1.307534
+    ## [2,]  0.7220340  1.1426576 0.6124631 -1.013255
+
+## References
+
+This is inspired by the `mcreplicate` function from the
+[rethinking](https://github.com/rmcelreath/rethinking) package.
